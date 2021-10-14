022ebb9ef4858ea73c98aed47f88135d8ae51f68
Eli <eli.vercollier@gmail.com>
2021-10-15 00:54:29 +0200

    chore: Create login page

---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,50 @@
+// package
+import React from "react";
+import {  useHistory  } from "react-router";
+
+// component
+import DisconnectedTemplate from "../../shared/component/Template/DisconnectedTemplate/DisconnectedTemplate";
+import { Form, Button, Divider } from 'rsuite';
+
+// style
+import "rsuite/dist/rsuite.min.css";
+import '../../shared/style/formStyle.css';
+
+const Login = () => {
+    const history = useHistory();
+
+    function redirect(){
+        const path = `/inscription`;
+        history.push(path);
+    }
+
+    return (
+    <DisconnectedTemplate>
+        <div className="form">
+            <Form>
+                <Form.Group controlId="email">
+                    <Form.ControlLabel>Identifiant</Form.ControlLabel>
+                    <div className="wrapped-tooltip">
+                        <Form.Control name="email" type="email" />
+                        <Form.HelpText tooltip>Votre adresse mail</Form.HelpText>
+                    </div>
+                </Form.Group>
+                <Form.Group controlId="password" >
+                    <Form.ControlLabel>Mot de passe</Form.ControlLabel>
+                    <Form.Control name="password" type="password" autoComplete="off" />
+                </Form.Group>
+                <Form.Group>
+                    <Button appearance="primary" className="button">Se connecter</Button>
+                </Form.Group>
+            </Form>
+            <Divider/>
+            <div className="switch">
+                <p>Pas encore de compte ?</p>
+                <Button  appearance="primary" onClick={redirect}>S'enregistrer</Button>
+            </div>
+        </div>
+    </DisconnectedTemplate>
+    )
+}
+
+export default Login;
