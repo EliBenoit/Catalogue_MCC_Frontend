f8304f4efbf856571d828fe614b583502043486f
Eli <eli.vercollier@gmail.com>
2021-10-15 00:52:20 +0200

    chore: Configuration

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+// packages
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
 
-function App() {
+// component
+import Login from './components/Login';
+import Register from './components/Register';
+/*import Home from './Home';
+import Profil from './Profil';
+import PrivateRoute from "./PrivateRoute/PrivateRoute";*/
+
+// style
+import 'rsuite/styles/index.less';
+
+/*
+          <PrivateRoute path="/">
+            <Home />
+          </PrivateRoute>
+          <PrivateRoute path="/mon-compte">
+            <Profil />
+          </PrivateRoute>
+          */
+
+const App = () => {
   return (
+    <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Switch>
+          <Route path="/connexion">
+            <Login />
+          </Route>
+          <Route path="/inscription">
+            <Register />
+          </Route>
+      </Switch>
     </div>
+    </Router>
   );
 }
 
